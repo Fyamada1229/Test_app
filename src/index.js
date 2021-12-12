dcbba8947ef00d15b39046eae836259b4f3742f0
Fyamada1229 <yamadafutosi1229@gmail.com>
2021-12-12 23:21:52 +0900

    練習

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const buttonText = { text: 'Click Me!' };
+  const labelText = "Enter name:";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const getTime = () => {
+    return (new Date()).toLocaleTimeString()
+  }
+
+  return (
+    <div>
+      <h1>Hi Jan I Love You. meet today</h1>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText.text}
+      </button>
+      {getTime()}
+    </div>
+  );
+};
+
+ReactDom.render(<App />, document.getElementById("root"));
